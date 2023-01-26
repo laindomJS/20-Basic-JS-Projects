925691f870b85da2dc1b9e487eda55b3925c0972
laindomJS <adrijar21@gmail.com>
2023-01-26 16:36:02 -0400

    feat: added funcionality - fix bugs

---
+++ 3. Planetarian Weight/assets/main.js
@@ -0,0 +1,34 @@
+const $ = (selector) => document.querySelector(selector); // HELPER
+
+// DOM ELEMENTS
+const userWeight = $('#weight');
+const userSelection = $('#planets');
+const button = $('#btn');
+const message = $('#result');
+// 
+
+const earthGravity = 9.8;
+const planets = {
+  'earth': earthGravity,
+  'mars' : 3.7,
+  'jupiter': 24.7,
+  'saturn': 10.4,
+}
+
+let weight;
+let planet;
+let result;
+
+userWeight.addEventListener('input', (evt) => {
+  weight = parseInt(evt.target.value);
+})
+
+userSelection.addEventListener('change', (evt) => {
+  planet = evt.target.value;
+})
+
+button.addEventListener('click', () => {
+  result = weight * planets[planet]; 
+  message.innerHTML = `Your weight in ${planets[planet]} is ${result.toLocaleString()}`;
+  console.log(result.toLocaleString());
+})
