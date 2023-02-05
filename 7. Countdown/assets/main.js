a805693d6d12647001d0b06443dedfa58a6e6652
laindomJS <adrijar21@gmail.com>
2023-02-05 10:55:51 -0400

    feat: project finished

--- 7. Countdown/assets/main.js
+++ 7. Countdown/assets/main.js
@@ -14,7 +14,7 @@ let interval = setInterval(function(){
 
     //CALCULATE DAYS, HOUR, MINUTES & SECONDS
     let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((distance % (1000 * 60 * 60 * 24 )) / (1000 * 60 * 60));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((distance % (1000 * 60 )) / (1000));
 
